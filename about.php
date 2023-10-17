e0e10153804b06bb742f7a20a4e9493562092ab8
Hoamp <setiawanthomas43@gmail.com>
2023-10-17 09:45:06 +0700

    update about us

--- about.php
+++ about.php
@@ -34,7 +34,7 @@
     <!-- Service Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="row g-4">
+            <div class="row g-4 justify-content-center">
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="d-flex py-5 px-4">
                         <i class="fa fa-3x fa-headset text-primary flex-shrink-0"></i>
